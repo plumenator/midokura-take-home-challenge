cc1955fe122a74dcbc009e194c01e598d6324a66
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2013-04-09 01:08:55 +0900

    Attempt using recursion, and equalizing the depth.

--- TreeNode.c
+++ TreeNode.c
@@ -2,6 +2,41 @@ typedef struct TreeNode {
   TreeNode *parent;
 } TreeNode_t;
 
-TreeNode *findFirstCommonAncestor(TreeNode *other) {
-  return NULL;
+TreeNode *findFirstCommonAncestor(TreeNode *this, TreeNode *other) {
+  if (!this || !other)
+    return NULL;
+  
+  int thisDepth = depth(this);
+  int otherDepth = depth(other);
+
+  TreeNode *thisNew = (thisDepth <= otherDepth ? this : nthParent(this, thisDepth-otherDepth));
+  TreeNode *otherNew = (otherDepth <= thisDepth ? other : nthParent(other, otherDepth-thisDepth));
+  
+  return findFirstCommonAncestorHelper(thisNew, otherNew);
+}
+
+TreeNode *findFirstCommonAncestorHelper(TreeNode *this, TreeNode *other) {
+  if (this == other)
+    {
+      return this;
+    }
+  else
+    {
+      assert(this && other);
+      return findFirstCommonAncestorHelper(this->parent, other->parent);
+    }
+}
+
+int depth(TreeNode *node) {
+  if (!node)
+    return 0;
+  else
+    return 1 + depth(node->parent);
+}
+
+TreeNode *nthParent(TreeNode *node, int num) {
+  if (!num)
+    return node;
+  else
+    return nthParent(node->parent, num-1);
 }
